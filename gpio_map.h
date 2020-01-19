2d8ffc7f961db60ca49ad1b3e58948fa7910c63e
MacVIk <forinform95@mail.ru>
2020-01-19 18:39:39 +0300

    List of used peripheral

---
+++ gpio_map.h
@@ -0,0 +1,27 @@
+/*
+ * gpio_map.h
+ *
+ *  Created on: 15 џэт. 2020 у.
+ *      Author: Taras.Melnik
+ *
+ *      This file describes all GPIO with appropriate interfaces.
+ *      More detailed information about alternative functions
+ *      see in af_map.h file.
+ *
+ */
+
+#ifndef GPIO_MAP_H_
+#define GPIO_MAP_H_
+
+#include "af_map.h"
+
+/* TERM_UART */
+
+#define TERM_UART_PORT              GPIOA
+#define TERM_UART_PIN_TX            LL_GPIO_PIN_9
+#define TERM_UART_OTPUT_TYPE_TX     LL_GPIO_OUTPUT_PUSHPULL
+#define TERM_UART_PIN_RX            LL_GPIO_PIN_10
+#define TERM_UART_OTPUT_TYPE_RX     LL_GPIO_MODE_FLOATING
+
+
+#endif /* GPIO_MAP_H_ */
